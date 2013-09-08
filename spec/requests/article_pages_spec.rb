32b2ac27c009e98b3d2c027a26069c1b5ebbbb89
RafeHatfield <rafe.hatfield@suite101.com>
2013-09-07 23:09:02 -0700

    added remainder on restful actions:

--- spec/requests/article_pages_spec.rb
+++ spec/requests/article_pages_spec.rb
@@ -35,4 +35,42 @@ describe "Article pages" do
 
   end
 
+  describe "new article" do
+
+    let(:user) { FactoryGirl.create(:user) }
+
+    before do 
+      # sign_in user
+      visit new_article_path
+    end
+
+    let(:submit) { "Create Article" }
+
+    it { should have_title('New Article') }
+    it { should have_content('New Article') }
+
+    describe "with invalid information" do
+      it "should not create an article" do
+        expect { click_button submit }.not_to change(Article, :count)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_content('error') }
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Title", with: "Title"
+        fill_in "Body", with: "Body"
+      end
+
+      it "should create an article" do
+        expect { click_button submit }.to change(Article, :count).by(1)
+      end
+    end
+  end
+
 end
